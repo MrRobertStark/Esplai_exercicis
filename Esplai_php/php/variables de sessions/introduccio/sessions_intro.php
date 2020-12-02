806810d91d10815f559107d06e48f73bad3f2f83
Roberto Lagos <69302922+MrRobertStark@users.noreply.github.com>
2020-12-02 11:10:21 +0100

    intro_sessions

---
+++ Esplai_php/php/variables de sessions/introduccio/sessions_intro.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style type = "text/css"></style>
+    <title>Document</title>
+
+</head>
+<body class = "p-4">
+    <?php
+        $_SESSION["usuari"] = "Panfiloberto";
+        echo "Sessió creada correctament ";
+    ?>
+    <h1 class = "mb-4">Títol principal de la pàgina web</h1>
+
+    <!--JQuery and Bootstrap Bundle (includes Popper)-->
+    <script src="http://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script language= "javascript">
+        $(document).ready(function(){});
+    </script>
+</body>
+</html>
